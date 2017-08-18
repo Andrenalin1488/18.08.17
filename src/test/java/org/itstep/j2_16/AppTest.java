680b15e80d5c714ef5a13d66f98d307d4244e3fc
Student <student@lan.itstep.kiev.ua>
2017-08-18 20:45:44 +0300

    start a work

---
+++ src/test/java/org/itstep/j2_16/AppTest.java
@@ -0,0 +1,16 @@
+package org.itstep.j2_16;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+		//dsjfsdlkfksad
+
+
+        assertTrue("Assertion should be true", true);
+    }
+}
